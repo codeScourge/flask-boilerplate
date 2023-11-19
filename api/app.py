cf96adb357748eb426904fff7a643bf398c58082
codeScourge <code.scourge@gmail.com>
2023-11-19 13:12:19 +0100

    config file and error-handler

--- api/app.py
+++ api/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import werkzeug
+import json
 
 app = Flask(__name__)
+app.config.from_file("config.json", load=json.load)
 
-@app.route("/ping", methods=["GET"])
-def pingRoute():
-    return {"message": "PONG"}, 200
+@app.errorhandler(werkzeug.exceptions.HTTPException)
+def handleHTTPException(error):
+    app.logger.error(error.description)
+    return {"message": error.description}, error.code
 
 @app.route("/", methods=["GET"])
 def homeRoute():
     return render_template("home.html")
+
+@app.route("/ping", methods=["GET"])
+def pingRoute():
+    return {"message": "PONG"}, 200
